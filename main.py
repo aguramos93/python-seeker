031098291cfbb2f018297a63db708ca885017239
agu722 <agustin93_rh@hotmail.com>
2021-06-29 15:34:10 +0200

    Send positive and negative degrees in hex format by arguments. Control roll, pitch and yaw.

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-import seeker
-import argparse, time
+import sys, seeker, time
 
-def main(command):
-    global seeker 
+def main(mode_command, roll_command, pitch_command, yaw_command):
+    global seeker
     seeker = seeker.Seeker()
     try:
         seeker.open_serial()
         start_time = time.time()
+        roll_l, roll_h = seeker.calculate_angle(roll_command)
+        pitch_l, pitch_h = seeker.calculate_angle(pitch_command)
+        yaw_l, yaw_h = seeker.calculate_angle(yaw_command)
+        command = seeker.calculate_command(mode_command, roll_l, roll_h, pitch_l, pitch_h, yaw_l, yaw_h)
         seeker.send_command(command)
-        seeker.read_serial()
 
     except KeyboardInterrupt:
-        pass
-
-    except ValueError as e:
-        print(e)
-    finally:
-        seeker.close_serial() 
+        pass     
 
 if __name__== '__main__':
-    parser = argparse.ArgumentParser(description = 'This is a script to manage Seeker through serial communication', formatter_class = argparse.RawTextHelpFormatter)
-    parser._positionals.title = "Available commands"
-    parser.add_argument('command', nargs = '?', 
-        choices = ('motor-on', 'motor-off', 
-        'return-head', 'center-yaw', 'look-down', 
-        'yaw-45'),
-        default = 'center-yaw')
-    parser.add_argument('--custom', '-c', dest = 'custom_mode', nargs = 1, action = "store", help = "Send custom hex commamd", type=str)
-    args = parser.parse_args()
 
-    order = args.command
-
-    if args.custom_mode:
-        order = [int((args.custom_mode[0]),16)]
-
-    main(order)
+    mode = sys.argv[1]
+    roll = sys.argv[2]
+    pitch = sys.argv[3]
+    yaw = sys.argv[4]
+    main(int(mode), int(roll), int(pitch), int(yaw))
--- seeker.py
+++ seeker.py
@@ -1,41 +1,87 @@
 #!/usr/bin/python3
-import serial, binascii
+import serial
+import numpy as np
 
-MOTOR_ON = [0x3E, 0x45, 0x01, 0x46, 0x0B, 0x0B]
-MOTOR_OFF = [0x3E, 0x45, 0x01, 0x46, 0x0C, 0x0C]
-RETURN_HEAD = [0x3E, 0x45, 0x01, 0x46, 0x12, 0x12]
-CENTER_YAW = [0x3E, 0x45, 0x01, 0x46, 0x23, 0x23]
-LOOK_DOWN = [0x3E, 0x45, 0x01, 0x46, 0x11, 0x11]
+def dec2hex(dec):
+        
+        if dec >= 0:
+            hexa = hex(int(dec))
+        else:
+            absDec = abs(int(dec))
+            bina = bin(absDec)
+            bina = bina[2:].zfill(16)
 
-HEAD = [0xFF, 0x01, 0x0F, 0x10]
-CONTROL_MODE = [0x00, 0x00, 0x05] # mode yaw angle ref frame 
-ROLL = [0x00, 0x00, 0x00, 0x00] # speed + angle
-PITCH = [0x00, 0x00, 0x00, 0x00]
-YAW = [0x00, 0x00, 0x00, 0x08] # yaw 45º
-CHECKSUM = [0x0D]
-YAW_45 = HEAD + CONTROL_MODE + ROLL + PITCH + YAW + CHECKSUM # FF 01 0F 10 00 00 05 00 00 00 00 00 00 00 00 00 00 00 08 0D
+            inv_bina = ''
+            for i in bina:  
+                if i == '0':
+                    inv_bina += '1'
+                else:
+                    inv_bina += '0'
+
+            int_sum = int(inv_bina, 2) + int(1)
+            hexa = hex(int_sum)
+
+        return hexa
 
 class Seeker():
 
-    def __init__(self):
-        self.commands = {'motor-on': MOTOR_ON, 'motor-off': MOTOR_OFF, 
-        'return-head': RETURN_HEAD, 'center-yaw': CENTER_YAW, 'look-down': LOOK_DOWN, 
-        'yaw-45': YAW_45}
-        self.ser = ''
+    def calculate_angle(self, angle):
+        
+        angle /= 0.02197265625 # 1 unit: 0.02197265625 degree
+        hexAngle = dec2hex(angle)
+
+        if len(hexAngle) == 6:
+            low = hexAngle[4:6]
+            high = hexAngle[2:4]
+        elif len(hexAngle) == 5:
+            low = hexAngle[3:5]
+            high = "0" + hexAngle[2]
+        elif len(hexAngle) == 4:
+            low = hexAngle[2:4]
+            high = "00"
+        elif len(hexAngle) == 3:
+            low = "0" + hexAngle[2]
+            high = "00"
+        else:
+            print("ERROR: Introduce a correct angle!")
+        
+        low = int(low, 16)
+        high = int(high, 16)
+
+        low = np.uint8(low)
+        high = np.uint8(high)
+
+        return low, high
+
+    def calculate_command(self, mode, roll_l, roll_h, pitch_l, pitch_h, yaw_l, yaw_h):
+
+        HEAD = [0xFF, 0x01, 0x0F, 0x10]
+        CONTROL_MODE = [mode, mode, mode]
+        ROLL = [0x00, 0x00, roll_l, roll_h] # speed + angle
+        PITCH = [0x00, 0x00, pitch_l, pitch_h] # speed + angle
+        YAW = [0x00, 0x00, yaw_l, yaw_h] # speed + angle
+        CHECKSUM = CONTROL_MODE[0] + CONTROL_MODE[1] + CONTROL_MODE[2] + ROLL[0] + ROLL[1] + ROLL[2] + ROLL[3] + PITCH[0] + PITCH[1] + PITCH[2] + PITCH[3] + YAW[0] + YAW[1] + YAW[2] + YAW[3]
+        CHECKSUM = hex(CHECKSUM % 256)
+
+        if len(CHECKSUM) == 3:
+            checksum_m = "0" + CHECKSUM[2]
+        elif len(CHECKSUM) == 4:
+            checksum_m = CHECKSUM[2:4]
+        else:
+            print("ERROR: Checksum overflow, introduce another angle!")
+
+        checksum_m = np.uint8(int(checksum_m, 16))
+        CHECKSUM = [checksum_m]
+
+        command = HEAD + CONTROL_MODE + ROLL + PITCH + YAW + CHECKSUM
+
+        return command
+
+    def send_command(self, command):
+        self.ser.write(command)
 
     def open_serial(self, device_path='/dev/ttyUSB0', baudrate=115200):
         self.ser = serial.Serial(device_path, baudrate, timeout=1)
 
     def close_serial(self):
         self.ser.close()
-
-    def send_command(self, command):
-        self.command = command
-        if type(command) == str:
-            self.ser.write(self.commands[command])
-            
-    def read_serial(self):
-        if type(self.command) == list:
-            return binascii.hexlify(self.ser.readlines()[0]).decode('utf-8')
-        else:
-            return binascii.hexlify(self.ser.read()).decode('utf-8')
